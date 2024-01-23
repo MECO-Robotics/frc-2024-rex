d2ef5af0f2849324bbc99c8ddf5c4020fa992ce9
Nathaniel <restofnate@gmail.com>
2024-01-23 13:13:35 -0800

    even more changs

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -120,7 +120,8 @@ public class RobotContainer
    */
   public Command getAutonomousCommand()
   {
-      return new PathPlannerAuto("autos/3 note auto");
+      return new PathPlannerAuto("3 note auto");
+      //return new PathPlannerAuto("1 note auto far");
   }
 
   public void setDriveMode()
